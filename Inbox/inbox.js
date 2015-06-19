a034569f472c2503eac4e4c99256dad4e7237fe6
SandhyaGopchandani <s.gopchandani@gmail.com>
2015-06-19 12:19:24 +0500

    hhh

---
+++ Inbox/inbox.js
@@ -0,0 +1,209 @@
+var express = require('express')
+  , passport = require('passport')
+  , util = require('util')
+  , LocalStrategy = require('passport-local').Strategy;
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+//var flash    = require('connect-flash
+  var monk = require('monk');
+  var db = monk('localhost:27017/nodetest2');
+
+  var users = db.get('loginCollection');
+  var messages = {};
+
+var app = express();
+/*var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+*/
+
+//users.insert( [{ username: "bob", password: "secret" , email: "bob@example.com"} ,
+  //{ username: "joe", password: "birthday" , email: "joe@example.com"}] );
+
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+
+
+
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    
+    next();
+});
+
+
+
+function findById(id, fn) {
+   //for (var i = 0, len = users.length; i < len; i++) {
+    //var user = users[i];
+    var user = users.findOne({ _id: id }).on('success', function (doc) {
+      console.log("bhaaalla" + " ",doc);
+      return fn(null, doc);
+
+    });
+
+}
+
+
+function findByUsername(username, fn) {
+  //for (var i = 0, len = users.length; i < len; i++) {
+    //var user = users[i];
+    var user = users.findOne({ username: username },{},function(err,docs){
+      return fn(null,docs);
+    });
+
+
+}
+
+
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// configure Express
+app.use(cookieParser());
+//app.use(express.methodOverride());
+app.use(session({
+  
+  secret: 'keyboard cat'
+  //resave: true,
+  //saveUninitialized: true,
+  //cookie : { secure : false, maxAge : (40 * 60 * 60 * 1000)}, // 4 hours
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+  console.log("ser",user);
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  console.log(id,"id");
+  findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+
+// Use the LocalStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.  In the real world, this would query a database;
+//   however, in this example we are using a baked-in set of users.
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    // Find the user by username.  If there is no user with the given
+    // username, or the password is not correct, set the user to `false` to
+    // indicate failure and set a flash message.  Otherwise, return the
+    // authenticated `user`.
+    console.log(username);
+    findByUsername(username, function(err, user) {
+    console.log(user);
+
+      if (err) { return done(err); }
+      if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+      if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+      return done(null, user);
+    })
+  }
+));
+
+
+
+
+
+
+
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+//app.use(flash());
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+
+app.get('/account1', ensureAuthenticated, function(req, res){
+  res.send(req.user);
+
+});
+
+
+
+app.get('/login1', function(req, res){
+  res.send("login karrrlo!")
+});
+
+
+app.post('/login1', passport.authenticate('local', { failureRedirect: '/login1'}), function(req, res) {
+    
+// Set our collection
+ // users.remove({ name: 'sandhya' });
+
+    console.log("success",req.user);
+    console.log("s2",req.session);
+
+    res.redirect('/account1');
+
+
+  });
+
+//check this out. you added this methodd
+
+app.post('/', function(req, res) {
+      console.log(req.body);
+          var collection = req.db.get('loginCollection');
+
+    // Submit to the DB
+    
+    collection.insert( req.body, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            res.redirect('/');
+        }
+
+});
+
+
+});
+
+
+
+app.get('/logout1', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.listen(3000);
+
+
+function ensureAuthenticated(req, res, next) {
+  console.log("req.user",req.user,req.session);
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login1');
+}
